4c29ec634a1c9dd2f011f84fdca31d6ca48efa8a
chenaihua <1690133293@qq.com>
2024-07-16 16:47:08 +0800

    更新文档

---
+++ 2024/js/Promise.js
@@ -0,0 +1,55 @@
+// 手写Promise的实现
+
+const PROMISE_STATE = {
+    PENDING: 0,
+    FULFILLED: 1,
+    REJECTED: 2
+}
+
+class MyPromise {
+    // 创建一个变量用来存储Promise的结果 私有属性
+    #result
+    // 创建一个变量来记录Promise的状态 pending 0 fulfilled 1 rejected 2
+    #state = PROMISE_STATE.PENDING
+    // 形参
+    constructor(executor) {
+        executor(this.#resolve.bind(this), this.#reject);
+    }
+
+    // 私有的resolve()
+    #resolve(value) {
+        // 使用bind绑定this，否则this为undefined
+
+        // 禁止值被重复修改
+        if (this.#state !== PROMISE_STATE.PENDING) return
+        this.#result = value;
+        this.#state = 1;  // 数据填充成功
+    }
+
+    // 私有的reject()
+    #reject(reason) {
+
+    }
+
+    // 添加一个用来读取数据的then方法
+    then(onFulfilled, onRejected) {
+        if (this.#state === PROMISE_STATE.FULFILLED) {
+            // 目前来讲，then只能读取已经存储进Promise的数据，而不能读取异步存储的数据
+            onFulfilled(this.#result);
+        }
+    }
+}
+
+// 实参
+const mp = new MyPromise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('hello world');
+        resolve('hello');
+    }, 1000)
+})
+
+mp.then((result) => {
+    console.log('读取数据：' + result);
+})
+
+
